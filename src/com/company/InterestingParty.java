c7f2fa3d1e8652c771beee865e540cbded03f960
beatdjam <beat3526@gmail.com>
2018-08-26 02:52:19 +0900

    done InterestingParty

---
+++ src/com/company/InterestingParty.java
@@ -0,0 +1,22 @@
+package com.company;
+
+import java.util.stream.Stream;
+
+public class InterestingParty {
+    public int bestInvitation(String[] first, String[] second){
+        int max = 0;
+        for(int i = 0;i < first.length; i++){
+            int f = 0;
+            int s = 0;
+            for (int j = 0;j < first.length;j++){
+                if(first[i].equals(first[j])) f++;
+                if(first[i].equals(second[j])) f++;
+                if(second[i].equals(first[j])) s++;
+                if(second[i].equals(second[j])) s++;
+            }
+            int localMax = f > s ? f : s;
+            if(localMax > max) max = localMax;
+        }
+        return max;
+    }
+}
