eba1c2529605e91efdd3a09e7866b46b5fb0704a
beatdjam <beat3526@gmail.com>
2018-10-07 03:40:05 +0900

    解答用ベースファイル修正

--- AtCoder/ABC/base.java
+++ AtCoder/ABC/base.java
@@ -2,36 +2,19 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
-import java.util.*;
 import java.util.stream.Stream;
 
 public class Main {
     private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     private static PrintWriter pw = new PrintWriter(System.out);
     public static void main(String[] args) throws IOException {
-        // 解答ここから
-        // sample ABC094_C
-//         int N = nextInt();
-//         int[] X = arrayOfInt();
-//         int[] x = X.clone();
-//         Arrays.sort(x);
-//         // 中央値の境界を取る
-//         int floor = N/2 - 1;
-//         int border = x[floor];
-
-//         int a = x[floor + 1];
-//         int b = x[floor];
-//         for(int i = 0;i < N;i++){
-//             if(X[i] <= border){
-//                 println(a);
-//             }else{
-//                 println(b);
-//             }
-//         }
-
-        //解答ここまで
+        func();
         pw.flush();
         return;
+    }
+
+    static void func() throws IOException {
+        // 解答
     }
     // 入力受け取り
     static String next() throws IOException {return br.readLine();}
@@ -43,11 +26,12 @@ public class Main {
     static long getLong(String val) {return Long.parseLong(val);}
     static int getInt(String val) {return Integer.parseInt(val);}
     static int[] toIntArray(String[] array) {return Stream.of(array.clone()).mapToInt(Integer::parseInt).toArray();}
-    static  String joinToString(String[] array) {
+    static String joinToString(String[] array) {
         StringBuilder sb = new StringBuilder();
         for(int i = 0;i < array.length;i++) sb.append(array[i]);
         return sb.toString();
     }
+
     // 出力
     static void println(Object val){pw.println(val);}
 }
