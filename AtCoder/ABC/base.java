12d832345d00b48e9e997f2d5c994bb0f27028b4
beatdjam <beat3526@gmail.com>
2018-10-08 01:56:51 +0900

    add function
    指定の数の約数を求めるメソッドと指定の範囲の素数を求めるメソッドを追加

--- AtCoder/ABC/base.java
+++ AtCoder/ABC/base.java
@@ -2,6 +2,10 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class Main {
@@ -25,12 +29,40 @@ public class Main {
     // 変換
     static long getLong(String val) {return Long.parseLong(val);}
     static int getInt(String val) {return Integer.parseInt(val);}
-    static int[] toIntArray(String[] array) {return Stream.of(array.clone()).mapToInt(Integer::parseInt).toArray();}
+    static int[] toIntArray(String[] array) {
+        return Stream.of(array.clone()).mapToInt(Integer::parseInt).toArray();
+    }
     static String joinToString(String[] array) {
         StringBuilder sb = new StringBuilder();
         for(int i = 0;i < array.length;i++) sb.append(array[i]);
         return sb.toString();
     }
+    static int[] listToIntArray(List<Integer> list){
+        int[] ret = new int[list.size()];
+        for(int i = 0;i < ret.length;i++) {
+            ret[i] = list.get(i);
+        }
+        return ret;
+    }
+
+    // 計算系
+    static int[] getDivisor(int val){
+        List<Integer> divisor = new ArrayList<>();
+        for (int i = 1; i*i <= val; i++) {
+            if (val % i != 0) continue;
+            divisor.add(i);
+            divisor.add(val/i);
+        }
+        int[] result = listToIntArray(divisor);
+        Arrays.sort(result);
+        return result;
+    }
+    static int[] getPrime(int from, int to){
+        return IntStream.rangeClosed(from, to)
+                .filter(i -> IntStream.rangeClosed(2, (int)Math.sqrt(i)).allMatch(j -> i%j !=0))
+                .toArray();
+    }
+
 
     // 出力
     static void println(Object val){pw.println(val);}
