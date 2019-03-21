3a27faf558c87c2995336b1f5ed67dbdcb6190d9
beatdjam <beat3526@gmail.com>
2019-03-21 15:17:23 +0900

    Update Main.kt

--- AtCoder/ABC/Main.kt
+++ AtCoder/ABC/Main.kt
@@ -1,8 +1,4 @@
-import java.io.BufferedReader
-import java.io.InputStreamReader
 import java.io.PrintWriter
-
-val br = BufferedReader(InputStreamReader(System.`in`))
 val pw = PrintWriter(System.out)
 fun main(args : Array<String>) {
     func()
@@ -14,7 +10,7 @@ fun func() {
 }
 
 // 入力取得
-fun next() = br.readLine()!!
+fun next() = readLine()!!
 fun nextInt() = next().toInt()
 fun nextLong() = next().toLong()
 fun nextDouble() = next().toDouble()
